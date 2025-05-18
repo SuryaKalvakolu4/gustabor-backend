d75d5b1885445765e0c0f9c242f44b46c60c9f4f
Suryaprakash Reddy Kalvakolu <127802829+SuryaKalvakolu4@users.noreply.github.com>
2025-05-18 19:55:47 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Use absolute DB path to avoid SQLite path issues
 
-DATABASE_URL = "sqlite:///C:/Users/surya/Desktop/gustabor_project/gustabor.db"
+DATABASE_URL = "sqlite:///./gustabor.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
